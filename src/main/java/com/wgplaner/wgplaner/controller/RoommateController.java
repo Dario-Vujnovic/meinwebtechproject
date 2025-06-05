afbc65deb2f26d328cf502cff49e046269e3105b
Dario-Vujnovic <Dario.Vujnovic@Student.HTW-Berlin.de>
2025-06-05 17:39:36 +0200

    BugFix

--- src/main/java/com/wgplaner/wgplaner/controller/RoommateController.java
+++ src/main/java/com/wgplaner/wgplaner/controller/RoommateController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/roommates")
+@RequestMapping("/api/roommates")
 @CrossOrigin(origins = "*")
 public class RoommateController {
 
